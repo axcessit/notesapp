6533024d79f837989d8f929b128e4c28d100ef26
axcessit <axcessit@gmail.com>
2024-11-05 12:33:45 -0500

    changes for amplify

--- notesapp/src/App.jsx
+++ notesapp/src/App.jsx
@@ -32,19 +32,4 @@ function App() {
   )
 }
 
-
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={reactLogo} className="logo react" alt="React logo" />
-
-        <h1>Hello from Amplify</h1>
-      </header>
-    </div>
-  );
-}
-
-export default App;
-
+export default App
